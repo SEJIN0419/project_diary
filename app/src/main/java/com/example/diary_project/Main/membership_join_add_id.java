58ce573d38183bc09fc44078674040fbce1fb0d5
SEJIN0419 <ksj4019113@gmail.com>
2023-01-09 23:50:20 +0900

    두번째 업데이트 버튼 배경색 변겅, 글꼴 설정, 가입창 비밀번호 입력창 추가

--- app/src/main/java/com/example/diary_project/Main/membership_join_add_id.java
+++ app/src/main/java/com/example/diary_project/Main/membership_join_add_id.java
@@ -2,15 +2,26 @@ package com.example.diary_project.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.diary_project.R;
 
 public class membership_join_add_id extends AppCompatActivity {
-
+    Button next_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership_join_add_id);
+        next_info = findViewById(R.id.next_info);
+        next_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_membership_password = new Intent(membership_join_add_id.this, membership_join_add_password.class);
+                startActivity(go_membership_password);
+            }
+        });
     }
 }
